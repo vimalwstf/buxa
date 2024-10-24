594b053967e616eaa46ca917e33ec4b51c64d414
Vimal Saraswat <vimal@thewasserstoff.com>
2024-10-24 18:22:22 +0530

    Add loading on signup button

--- src/components/GoogleSignup.tsx
+++ src/components/GoogleSignup.tsx
@@ -1,4 +1,5 @@
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 
 export const handleSignIn = async () => {
@@ -6,16 +7,27 @@ export const handleSignIn = async () => {
 };
 
 export default function GoogleSignup() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <button
-      onClick={handleSignIn}
+      onClick={() => {
+        setLoading(true);
+        handleSignIn();
+      }}
       // onClick={() => console.log("clicked")}
       className="flex items-center gap-1 sm:gap-2 sm:text-lg font-medium p-1 sm:px-2 sm:py-1 md:px-4 md:py-2 "
     >
       <FcGoogle className="drop-shadow-2xl drop" />
       <div className="flex items-center justify-center">
-        <span>SignIn&nbsp;</span>
-        <span className="hidden sm:inline text-nowrap"> with Google</span>
+        {loading ? (
+          <span>Signing in...</span>
+        ) : (
+          <>
+            <span>SignIn&nbsp;</span>
+            <span className="hidden sm:inline text-nowrap"> with Google</span>
+          </>
+        )}
       </div>
     </button>
   );
