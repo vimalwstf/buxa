d7955dbf6ca88bc8ef93a2338803deda4005678b
Vimal Saraswat <vimal@thewasserstoff.com>
2024-10-26 14:22:01 +0530

    Update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,56 +1,23 @@
-"use client";
-import { useSession } from "next-auth/react";
-import Dashboard from "@/components/Dashboard";
-import { useEffect, useState } from "react";
-import { logIn, logOut } from "@/lib/user/userSlice";
-import { useAppDispatch } from "@/lib/hooks";
-import { signOut } from "next-auth/react";
-import axios from "axios";
-import Loader from "@/components/Loader";
-// import Dashboard from "@/components/dashboard/Dashboard";
+import Alerts from "@/components/dashboard/Alerts";
+import Sidebar from "@/components/dashboard/Sidebar";
+import Navbar from "@/components/dashboard/Navbar";
+import History from "@/components/dashboard/History";
+import Stats from "@/components/dashboard/Stats";
 
-export default function Home() {
-  const { data: session } = useSession();
-  const dispatch = useAppDispatch();
-  const accessToken = session?.user?.accessToken;
-  const [isLoading, setIsLoading] = useState(true);
-
-  //user fetch
-  useEffect(() => {
-    const fetchUser = async () => {
-      // console.log("accessToken", accessToken);
-      if (accessToken) {
-        try {
-          const response = await axios.get(
-            `${process.env.NEXT_PUBLIC_SOURCE_URL}/user`,
-            {
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "ngrok-skip-browser-warning": true,
-              },
-            },
-          );
-          if (response?.data?.status) {
-            dispatch(logIn(response?.data?.data));
-          } else {
-            if (response.status === 400) {
-              dispatch(logOut());
-              await signOut();
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        } finally {
-          setIsLoading(false);
-        }
-      } else {
-        setIsLoading(false); // Set loading to false once the check is done
-      }
-    };
-    fetchUser();
-  }, [accessToken, dispatch]);
-  if (isLoading) {
-    return <Loader />;
-  }
-  return <Dashboard />;
+export default function Dashboard() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <div className=" flex">
+          <div className="flex-1 flex flex-col">
+            <Stats />
+            <History />
+          </div>
+          <Alerts />
+        </div>
+      </div>
+    </div>
+  );
 }
