143b7e241c532bd38033544874338af5949fbd85
Vimal Saraswat <vimal@thewasserstoff.com>
2024-10-29 14:07:29 +0530

    Update form component

--- src/components/sidebar/Form.tsx
+++ src/components/sidebar/Form.tsx
@@ -18,12 +18,14 @@ export default function Form({
   return (
     <form
       onSubmit={onSubmit}
-      className="element h-[90vh] pb-32 sm:min-h-[87vh] overflow-y-scroll w-full rounded-md border-2 border-primary-light bg-primary-light p-4"
+      className="element h-dvh md:h-[calc(100dvh-6rem)] flex flex-col justify-between overflow-y-scroll w-full rounded-md border-2 border-primary-light bg-primary-light p-4"
     >
-      <h2 className="text-[1.3rem] font-semibold mb-4 text-text-light">
-        {heading}
-      </h2>
-      {children}
+      <div>
+        <h2 className="text-[1.3rem] font-semibold mb-4 text-text-light">
+          {heading}
+        </h2>
+        {children}
+      </div>
       <SidebarSubmit
         variant={variant}
         disabled={!allFilled}
