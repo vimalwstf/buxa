02bc4cc66e7537e2a1483226b6bf685354ce6837
sahilworkdev <sahil.malik@thewasserstoff.com>
2024-10-29 12:38:05 +0530

    delete extra Dashboard component, fix loader on /dashboard

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center py-32 space-x-2 h-[100vh] bg-custom-gradient">
+    <div className="flex justify-center items-center py-32 space-x-2 h-[100vh] ">
       <div className="w-4 h-4 bg-zinc-500 rounded-full animate-pulse"></div>
       <div className="w-4 h-4 bg-zinc-500 rounded-full animate-pulse delay-200"></div>
       <div className="w-4 h-4 bg-zinc-500 rounded-full animate-pulse delay-400"></div>
