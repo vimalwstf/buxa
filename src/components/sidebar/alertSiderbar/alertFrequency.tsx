2590e4145cbe1b0936a345f8c72f088cff5c53d5
Vimal Saraswat <vimal@thewasserstoff.com>
2024-11-02 14:19:24 +0530

    Fix alert frequency timePicker

--- src/components/sidebar/alertSiderbar/alertFrequency.tsx
+++ src/components/sidebar/alertSiderbar/alertFrequency.tsx
@@ -5,10 +5,11 @@ import { FaCirclePlus } from "react-icons/fa6";
 import WeekDayPicker from "./weekDayPickerDropdown";
 import DatePicker from "./datePicker";
 
+const maxTimeSlots = 2;
+
 export default function AlertFrequency() {
   const [selectedFrequency, setSelectedFrequency] = useState("daily");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
+  const [timeSlots, setTimeSlots] = useState([{ startTime: "", endTime: "" }]);
   const [weekDay, setWeekDay] = useState("");
   const [selectedDate, setSelectedDate] = useState<number | null>(null);
 
@@ -16,49 +17,84 @@ export default function AlertFrequency() {
     selectedFrequency,
     selectedDate,
     weekDay,
-    startTime,
-    endTime,
+    timeSlots,
   };
+
   console.log(schedule);
+
   const onDateChange = (value: number) => {
     setSelectedDate(value);
   };
-  const onStartTimeChange = (value: string) => {
-    setStartTime(value);
+
+  const onTimeChange = (
+    index: number,
+    type: "start" | "end",
+    value: string
+  ) => {
+    const updatedSlots = timeSlots.map((slot, i) => {
+      if (i === index) {
+        return {
+          ...slot,
+          [type === "start" ? "startTime" : "endTime"]: value,
+        };
+      }
+      return slot;
+    });
+    setTimeSlots(updatedSlots);
   };
-  const onEndTimeChange = (value: string) => {
-    setEndTime(value);
-  };
+
   const handleFrequencyChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     setSelectedFrequency(event.target.value);
   };
+
   const handleWeekDayChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setWeekDay(event.target.value);
   };
+
+  const addTimeSlot = () => {
+    setTimeSlots([...timeSlots, { startTime: "", endTime: "" }]);
+  };
+
+  const deleteTimeSlot = (index: number) => {
+    if (timeSlots.length === 1) return;
+    const updatedSlots = timeSlots.filter((_, i) => i !== index);
+    setTimeSlots(updatedSlots);
+  };
+
   return (
     <div className="mb-4">
       <label className="block text-text-third font-medium mb-2">
         Alert frequency
       </label>
+
       <Frequency
         selectedFrequency={selectedFrequency}
         handleFrequencyChange={handleFrequencyChange}
       />
-      <Time
-        startTime={startTime}
-        endTime={endTime}
-        onStartTimeChange={onStartTimeChange}
-        onEndTimeChange={onEndTimeChange}
-      />
+
+      {timeSlots.map((slot, index) => (
+        <Time
+          key={index}
+          startTime={slot.startTime}
+          endTime={slot.endTime}
+          onDelete={() => deleteTimeSlot(index)}
+          onStartTimeChange={(value) => onTimeChange(index, "start", value)}
+          onEndTimeChange={(value) => onTimeChange(index, "end", value)}
+        />
+      ))}
+
       <button
         type="button"
-        className="flex items-center gap-2 bg-slate-300 text-black px-4 py-2 rounded w-full justify-center mb-4"
+        onClick={addTimeSlot}
+        disabled={timeSlots.length === maxTimeSlots}
+        className="flex items-center gap-2 bg-slate-300 text-black px-4 py-2 rounded w-full justify-center mb-4 disabled:cursor-not-allowed"
       >
         <FaCirclePlus />
         <span>Add more timing</span>
       </button>
+
       {selectedFrequency === "weekly" && (
         <WeekDayPicker
           selectedDay={weekDay}
