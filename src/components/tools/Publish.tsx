edc62e8a524e92cdfbe48a83849a8993c3083195
sahilworkdev <sahil.malik@thewasserstoff.com>
2024-11-07 17:21:40 +0530

    publish model

--- src/components/tools/Publish.tsx
+++ src/components/tools/Publish.tsx
@@ -1,7 +1,9 @@
 import useClickOutside from "@/hooks/useClickOutisde";
+import useLocalStorage from "@/hooks/useLocalStorage";
 import { DocumentInfo } from "@/types/type";
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
 import { useRef, useState } from "react";
-import { IoShareSocialSharp } from "react-icons/io5";
 
 type OptionsModalProps = {
   docData: DocumentInfo;
@@ -9,67 +11,148 @@ type OptionsModalProps = {
 
 function Publish({ docData }: OptionsModalProps) {
   const [modalOpen, setModalOpen] = useState(false);
-
+  const [isPublishing, setIsPublishing] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
 
+  const { value: user } = useLocalStorage("user", { accessToken: "" });
+  const accessToken = user?.accessToken;
+
   const closeModal = () => {
-    setModalOpen(false);
+    if (!isPublishing) {
+      setModalOpen(false);
+    }
   };
   useClickOutside(modalRef, closeModal);
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (accessToken) {
+      setIsPublishing(true);
+      const formData = new FormData(e.target as HTMLFormElement);
+      const metadata = {
+        apiKey: formData.get("api-key") as string,
+        postOn: formData.get("site") as string,
+        ghostURL: formData.get("url") as string,
+        content: docData.name,
+        metadata: docData.metadata,
+        keyword: docData.keyword,
+        tag: docData.tag,
+      };
+
+      console.log("---------------blog---------------", metadata);
+
+      try {
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_SOURCE_URL}/documents/blog`,
+          { data: metadata },
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        if (response?.data?.status) {
+          enqueueSnackbar("Document published successfully", {
+            variant: "success",
+            anchorOrigin: {
+              vertical: "top",
+              horizontal: "center",
+            },
+          });
+          setModalOpen(false); // Close modal after successful publish
+        }
+      } catch (err) {
+        const error = err as any;
+        enqueueSnackbar(
+          `Failed to publish document: ${
+            error?.response?.data?.error as string
+          }`,
+          {
+            variant: "error",
+            anchorOrigin: {
+              vertical: "top",
+              horizontal: "center",
+            },
+          }
+        );
+      } finally {
+        setIsPublishing(false);
+      }
+    }
+  };
+
   return (
-    <>
-      <div className="relative" ref={modalRef}>
-        <button
-          type="button"
-          className="bg-secondary-default text-white flex items-center gap-2 top-10 px-4 py-2 text-sm rounded-md font-medium"
-          onClick={(e) => {
-            e.stopPropagation();
-            setModalOpen(!modalOpen);
-          }}
-        >
-          <IoShareSocialSharp />
-          <span>Share</span>
-        </button>
-        {modalOpen && (
+    <div>
+      <button
+        type="button"
+        className="bg-secondary-default text-white px-4 py-2 text-sm rounded-md font-medium"
+        onClick={() => setModalOpen(true)}
+      >
+        Publish to Site
+      </button>
+      {modalOpen && (
+        <>
           <div
-            className="absolute top-full right-0 rounded-lg flex flex-col gap-2 overflow-y-auto px-4 py-2 bg-secondary-default shadow-lg z-10"
+            className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-10"
+            //  ref={modalRef}
+          ></div>
+          <div
+            className="fixed inset-0 flex items-center justify-center z-20 "
             onClick={(e) => e.stopPropagation()}
           >
-            <form className="w-fit h-fit flex gap-2 flex-col cursor-pointer z-10">
-              <input
-                type="text"
-                placeholder="Enter API here"
-                name="api-key"
-                className="p-2 rounded-md outline-none text-black"
-              />
-              <select
-                name="site"
-                className="p-2 rounded-md outline-none text-black "
-              >
-                <option value="" disabled selected hidden>
-                  Choose a site
-                </option>
-                <option value="1">Ghost</option>
-                <option value="2">Wordpress</option>
-              </select>
-              <input
-                type="url"
-                placeholder="Enter URL here"
-                name="url"
-                className="p-2 rounded-md outline-none text-black"
-              />
+            <div className="relative bg-primary-light rounded-lg flex flex-col gap-2 p-6 pt-10 shadow-lg w-96 border-2 border-gray-200">
               <button
-                type="submit"
-                className="bg-primary-default text-white p-2 rounded-md hover:font-bold"
+                className="absolute top-0 px-2 py-1  right-4 text-white text-xl font-bold hover:text-red-500 duration-200"
+                onClick={closeModal}
               >
-                Publish
+                &times;
               </button>
-            </form>
+              <div className="text-center">
+                <h1 className="px-2 mb-6 text-3xl font-bold">Enter your blog site details below</h1>
+              </div>
+              <form
+                onSubmit={handleSubmit}
+                className="w-full h-full flex gap-4 flex-col cursor-pointer "
+              >
+                <input
+                  required
+                  type="text"
+                  placeholder="Enter API key here"
+                  name="api-key"
+                  className="p-2 rounded-md outline-none text-black"
+                />
+                <select
+                  name="site"
+                  required
+                  className="p-2 rounded-md outline-none text-black"
+                >
+                  <option value="" disabled selected hidden>
+                    Choose a site
+                  </option>
+                  <option value="ghost">Ghost</option>
+                  <option value="wordpress">Wordpress</option>
+                </select>
+                <input
+                  type="url"
+                  required
+                  placeholder="Enter URL here"
+                  name="url"
+                  className="p-2 rounded-md outline-none text-black mb-4"
+                />
+                <button
+                  type="submit"
+                  className="bg-primary-green text-black p-2 rounded-md hover:scale-105 duration-200"
+                  disabled={isPublishing}
+                >
+                  {isPublishing ? "Publishing..." : "Publish"}
+                </button>
+              </form>
+            </div>
           </div>
-        )}
-      </div>
-    </>
+        </>
+      )}
+    </div>
   );
 }
 
