464094775fd13a5abe007380978981007953be03
Vimal Saraswat <vimal@thewasserstoff.com>
2024-11-08 15:23:28 +0530

    Update settings

--- src/app/(tools)/settings/page.tsx
+++ src/app/(tools)/settings/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { useState } from "react";
+
 import PublishSubmit from "@/components/ui/PublishSubmit";
-import React from "react";
 
 const data = {
   ghost: {
@@ -16,21 +19,77 @@ const data = {
 };
 
 const Page = () => {
+  const [selected, setSelected] = useState<"ghost" | "wordpress">("ghost");
+
   return (
     <div className="w-full flex flex-col items-center justify-start">
       <h1 className="text-center text-4xl font-bold text-text-light">
         Settings
       </h1>
+      <>
+        <h3 className="text-text-light">Select a blogging platform</h3>
+        <div className="flex gap-2">
+          <label className="text-text-light">
+            <span>Wordpress</span>
+            <input
+              required
+              type="radio"
+              name="radio"
+              value="wordpress"
+              onChange={() => setSelected("wordpress")}
+            />
+          </label>
 
+          <label className="text-text-light">
+            <span>Ghost</span>
+            <input
+              required
+              type="radio"
+              name="radio"
+              value="ghost"
+              onChange={() => setSelected("ghost")}
+            />
+          </label>
+        </div>
+      </>
       <form className="element w-full max-w-md aspect-video flex flex-col justify-between rounded-md bg-primary-light p-4">
-        <div>
-          <h2 className="text-[1.3rem] font-semibold mb-4 text-text-light"></h2>
-          <input type="text" placeholder="Enter your name" />
+        <div className="flex flex-col gap-2">
+          {selected === "ghost" && <Ghost />}
+          {selected === "wordpress" && <Wordpress />}
         </div>
         <PublishSubmit disabled={false} loading={false} />
       </form>
     </div>
   );
 };
+
+function Ghost() {
+  return (
+    <>
+      <input required name="apiKey" type="text" placeholder="Enter api key" />
+      <input required name="url" type="url" placeholder="Enter your url" />
+    </>
+  );
+}
+
+function Wordpress() {
+  return (
+    <>
+      <input
+        required
+        name="username"
+        type="text"
+        placeholder="Enter username"
+      />
+      <input
+        required
+        name="password"
+        type="text"
+        placeholder="Enter password"
+      />
+      <input required name="url" type="url" placeholder="Enter your url" />
+    </>
+  );
+}
 
 export default Page;
